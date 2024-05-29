13afe243f461df1663f6e0494081e31c8a4224fe
fmfergo <fergofm@gmail.com>
2024-05-29 11:26:33 +0100

    Format

--- guess_number.php
+++ guess_number.php
@@ -2,12 +2,15 @@
 
 <?php
 
-if (!isset($_SESSION['numberToGuess'])) {
-  $_SESSION['numberToGuess'] = rand(1, 10);
-  $_SESSION['attempts'] = 3;
-}
+  if (!isset($_SESSION['numberToGuess'])) {
+    $_SESSION['numberToGuess'] = rand(1, 10);
+    $_SESSION['attempts'] = 3;
+  }
 
   // The use of a maximum number of attemps is not really implemented
+  // Currently you have to go back to the original form on the browser
+  // in order to enter a new guess, and that seems to make 
+  // $_SESSION for gets the previous number of attempts
   if ($_SESSION['attempts'] > 0) {
     // Reduce the numbers of attemps left by 1
     $attempts = $_SESSION['attempts'];
@@ -21,6 +24,7 @@ if (!isset($_SESSION['numberToGuess'])) {
     if ($guess == $_SESSION['numberToGuess']) {echo "You answered ".$guess.". This is the correct answer!";}
     else if ($guess < $_SESSION['numberToGuess']) {echo "You answered ".$guess.". The correct answer is higher. You have ".$_SESSION['attempts']." left";}
     else if ($guess > $_SESSION['numberToGuess']) {echo "You answered ".$guess.". The correct answer is lower. You have ".$_SESSION['attempts']." left";}
+  
   } else {
     echo "Sorry! You ran out of attemps";
   }
