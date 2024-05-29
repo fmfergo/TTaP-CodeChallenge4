3d2bd0774a38f8ef30a0ab5e7758b6fc4605a849
fmfergo <fergofm@gmail.com>
2024-05-29 11:33:17 +0100

    Format

--- guess_number.php
+++ guess_number.php
@@ -21,11 +21,38 @@
     $guess = $_POST['guess'];
 
     // Compare the number entered with the number to guess
-    if ($guess == $_SESSION['numberToGuess']) {echo "You answered ".$guess.". This is the correct answer!";}
-    else if ($guess < $_SESSION['numberToGuess']) {echo "You answered ".$guess.". The correct answer is higher. You have ".$_SESSION['attempts']." left";}
-    else if ($guess > $_SESSION['numberToGuess']) {echo "You answered ".$guess.". The correct answer is lower. You have ".$_SESSION['attempts']." left";}
+    if ($guess == $_SESSION['numberToGuess']) {
+      echo "You answered ".$guess.". This is the correct answer!";
+    }
+    else if ($guess < $_SESSION['numberToGuess']) {
+      echo "You answered ".$guess.". The correct answer is higher. You have ".$_SESSION['attempts']." left";
+    }
+    else if ($guess > $_SESSION['numberToGuess']) {
+      echo "You answered ".$guess.". The correct answer is lower. You have ".$_SESSION['attempts']." left";
+    }
   
   } else {
     echo "Sorry! You ran out of attemps";
   }
 ?>
+
+<!DOCTYPE html>
+
+<head>
+
+  <title>Guessing the number</title>
+
+</head>
+
+<body>
+
+  <h1>Guess a number between 1 and 10!</h1>
+
+  <form action="guess_number.php" method="post">
+    <!-- only numbers are valid inputs -->
+    <!-- TODO: Disallow numbers out of the range  -->
+    <input type="number" name="guess">
+
+    <input type="submit" value="Check!">
+
+</body>
